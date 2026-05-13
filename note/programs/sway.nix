4eabec9b7d19ce77f95dcdfb4079a08e3f91c911
lewelove <lewelove@proton.me>
2026-05-13 22:12:49 +0300

    2026-05-13 19:12 UTC

--- note/programs/sway.nix
+++ note/programs/sway.nix
@@ -1,7 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, username, dot, ... }:
 
 {
-
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
@@ -16,4 +15,10 @@
 
   services.displayManager.sessionPackages = [ pkgs.sway ];
 
+  home-manager.users.${username} = { config, ... }: {
+    home.file = {
+      ".config/sway".source = config.lib.file.mkOutOfStoreSymlink "${dot}/.config/sway";
+      ".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${dot}/.config/waybar";
+    };
+  };
 }
--- note/programs/thunar.nix
+++ note/programs/thunar.nix
@@ -6,8 +6,4 @@
   services.gvfs.enable = true;
   services.tumbler.enable = true;
   services.udisks2.enable = true;
-
-  home-manager.users.${username} = { config, ... }: {
-    home.file.".config/Thunar".source = config.lib.file.mkOutOfStoreSymlink "${dot}/.config/Thunar";
-  };
 }
