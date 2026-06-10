07af1a9486abd939d0c34858172ceb44e52f657a
lewelove <lewelove@proton.me>
2026-06-10 18:54:07 +0300

    2026-06-10 15:54 UTC

--- hosts/home/services/xremap.nix
+++ hosts/home/services/xremap.nix
@@ -10,7 +10,7 @@
         After = [ "default.target" ];
       };
       Service = {
-        ExecStart = "${pkgs.xremap}/bin/xremap --device \"SEM USB Keyboard\" xremap --device \"0Xbox Wireless Controller\" ${config.home.homeDirectory}/.config/xremap/config.yml";
+        ExecStart = "${pkgs.xremap}/bin/xremap --device \"SEM USB Keyboard\" --device \"Xbox Wireless Controller\" ${config.home.homeDirectory}/.config/xremap/config.yml";
         Restart = "always";
         RestartSec = "3s";
       };
