ffc315537aa020614678753ef51c83e8c06920f0
lewelove <lewelove@proton.me>
2026-07-01 15:23:06 +0300

    2026-07-01 12:23 UTC

--- hosts/lab/network/adguardhome.nix
+++ hosts/lab/network/adguardhome.nix
@@ -7,16 +7,7 @@
   };
 
   networking.firewall = {
-    allowedTCPPorts = [ 3000 ];
-    extraCommands = ''
-      VPN_IPS=(
-        "185.200.118.4"
-        "185.200.118.5"
-      )
-      for ip in "''${VPN_IPS[@]}"; do
-        iptables -A INPUT -p udp -s "$ip" --dport 53 -j ACCEPT
-        iptables -A INPUT -p tcp -s "$ip" --dport 53 -j ACCEPT
-      done
-    '';
+    allowedTCPPorts = [ 3000 53 ];
+    allowedUDPPorts = [ 53 ];
   };
 }
