156d8bf5c431545a130fbeaf1e034e3ab5ad21a1
lewelove <lewelove@proton.me>
2026-08-09 23:56:16 +0300

    2026-08-09 20:56 UTC

--- hosts/home/network/d/arch-vm-mount.nix
+++ hosts/home/network/d/arch-vm-mount.nix
@@ -2,14 +2,13 @@
 
 {
   fileSystems."/home/${username}/virtual/arch" = {
-    device = "arch@127.0.0.1:/home/arch";
+    device = "arch@192.168.1.100:/home/arch";
     fsType = "fuse.sshfs";
     options = [
       "x-systemd.automount"
       "noatime"
       "_netdev"
       "port=2222"
-      "ProxyJump=lewelove@192.168.1.100"
       "IdentityFile=/home/${username}/.ssh/id_ed25519"
       "StrictHostKeyChecking=no"
       "UserKnownHostsFile=/dev/null"
